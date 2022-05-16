2feb3145abc79d4dda74e68055495e5222fc3bc1
Nguyen Minh Quang <63718153+nbtpj@users.noreply.github.com>
2022-05-16 10:23:45 +0700

    edit postdata v5 (fix_bug)

--- lib/ui/components/image_provider.dart
+++ lib/ui/components/image_provider.dart
@@ -3,14 +3,17 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 ImageProvider any_image(String url, bool? isNet) {
+  /// hàm này nhận vào 1 url và trả về 1 ImageProvider có thể từ Net hoặc File
+  /// , nếu isNet == null
   ImageProvider rs;
   if (isNet != null) {
     isNet ? rs = NetworkImage(url) : rs = FileImage(File(url));
   } else {
-    try{
+    try {
+      var f = File(url);
+      rs = FileImage(f);
+    } catch (e) {
       rs = NetworkImage(url);
-    } catch (e){
-      rs = FileImage(File(url));
     }
   }
   return rs;
