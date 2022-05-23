fcd1d80ed8393cfb744796ba5b94207ba829aa56
Nguyen Minh Quang <63718153+nbtpj@users.noreply.github.com>
2022-05-23 21:12:52 +0700

    merge quang

--- lib/ui/screens/comment_on_post/comment_food.dart
+++ lib/ui/screens/comment_on_post/comment_food.dart
@@ -14,6 +14,7 @@ class CommentFood extends StatefulWidget {
   @override
   _CommentFoodState createState() => _CommentFoodState();
 }
+// todo: update realtime + sort by time
 
 class _CommentFoodState extends State<CommentFood> {
   @override
