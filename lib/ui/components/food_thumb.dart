6fb1f59b0bc647229fdd0279ab78a68cbad73bed
Nguyen Minh Quang <63718153+nbtpj@users.noreply.github.com>
2022-06-13 23:30:42 +0700

    fix bug

--- lib/ui/components/food_thumb.dart
+++ lib/ui/components/food_thumb.dart
@@ -7,107 +7,103 @@ import 'package:foodnet_01/util/data.dart';
 import 'package:foodnet_01/util/entities.dart';
 import 'package:foodnet_01/util/global.dart';
 
-Widget build_a_food_thumb(BuildContext context, PostData food) {
-  return GestureDetector(
-    onTap: () {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => PostDetailView(food: food)));
-    },
-    child: Stack(
-      children: [
-        Container(
-          margin: EdgeInsets.fromLTRB(
-              SizeConfig.screenWidth / 34.25,
-              SizeConfig.screenHeight / 113.84,
-              SizeConfig.screenWidth / 34.25,
-              SizeConfig.screenHeight / 22.77),
-          height: SizeConfig.screenHeight / 3.105,
-          width: SizeConfig.screenWidth / 2.74,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30.0),
-              boxShadow: [
-                BoxShadow(
-                  offset: const Offset(0, 3),
-                  blurRadius: 6,
-                  color: Colors.black.withOpacity(0.3),
-                )
-              ]),
-          child: Stack(
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: SizeConfig.screenHeight / 6.83,
-                    width: SizeConfig.screenWidth / 2.74,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(food.outstandingIMGURL),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(30.0),
-                          topRight: Radius.circular(30.0)),
+Widget build_a_food_thumb_no_tap(PostData food) {
+  return Stack(
+    children: [
+      Container(
+        margin: EdgeInsets.fromLTRB(
+            SizeConfig.screenWidth / 34.25,
+            SizeConfig.screenHeight / 113.84,
+            SizeConfig.screenWidth / 34.25,
+            SizeConfig.screenHeight / 22.77),
+        height: SizeConfig.screenHeight / 3.105,
+        width: SizeConfig.screenWidth / 2.74,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30.0),
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(0, 3),
+                blurRadius: 6,
+                color: Colors.black.withOpacity(0.3),
+              )
+            ]),
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: SizeConfig.screenHeight / 6.83,
+                  width: SizeConfig.screenWidth / 2.74,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(food.outstandingIMGURL),
+                      fit: BoxFit.cover,
                     ),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0)),
                   ),
-                   Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: SizeConfig.screenHeight / 35,
-                            child: FittedBox(
-                                fit: BoxFit.fitWidth,
-                                child: Text(
-                                  food.title,
-                                  // softWrap: true,
-                                  style: TextStyle(
-                                      color: Colors.black54,
-                                      fontSize: SizeConfig.screenHeight / 36.15,
-                                      fontWeight: FontWeight.w700),
-                                )),
-                          )
-                          ,
-                          FutureBuilder<ProfileData?>(
-                              future: food.getOwner(),
-                              builder: (context, snap) => snap.hasData
-                                  ? SizedBox(
-                                  height: SizeConfig.screenHeight / 40,
-                                  child: FittedBox(
-                                      fit: BoxFit.fitWidth,
-                                      child: Text(snap.data?.name ?? None,
-                                          softWrap: true,
-                                          style: TextStyle(
-                                              color: Colors.green,
-                                              fontSize:
-                                                  SizeConfig.screenHeight / 40,
-                                              fontFamily: "Roboto"))))
-                                  : const SizedBox.shrink()),
-                          food.cateList.isNotEmpty
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: SizeConfig.screenHeight / 35,
+                        child: FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: Text(
+                              food.title,
+                              // softWrap: true,
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: SizeConfig.screenHeight / 36.15,
+                                  fontWeight: FontWeight.w700),
+                            )),
+                      )
+                      ,
+                      FutureBuilder<ProfileData?>(
+                          future: food.getOwner(),
+                          builder: (context, snap) =>
+                          snap.hasData
                               ? SizedBox(
                               height: SizeConfig.screenHeight / 40,
-                              child:FittedBox(
+                              child: FittedBox(
                                   fit: BoxFit.fitWidth,
-                                  child: Text(
-                                    food.cateList.join(','),
-                                    softWrap: true,
-                                    style: TextStyle(
-                                        color: Colors.black38,
-                                        fontSize:
-                                            SizeConfig.screenHeight / 42.69,
+                                  child: Text(snap.data?.name ?? None,
+                                      softWrap: true,
+                                      style: TextStyle(
+                                          color: Colors.green,
+                                          fontSize:
+                                          SizeConfig.screenHeight / 40,
+                                          fontFamily: "Roboto"))))
+                              : const SizedBox.shrink()),
+                      food.cateList.isNotEmpty
+                          ? SizedBox(
+                          height: SizeConfig.screenHeight / 40,
+                          child: FittedBox(
+                              fit: BoxFit.fitWidth,
+                              child: Text(
+                                food.cateList.join(','),
+                                softWrap: true,
+                                style: TextStyle(
+                                    color: Colors.black38,
+                                    fontSize:
+                                    SizeConfig.screenHeight / 42.69,
 
-                                        /// 16
-                                        fontWeight: FontWeight.w400),
-                                  )))
-                              : SizedBox.shrink(),
-                          Padding(
-                            padding: EdgeInsets.only(
-                                top: SizeConfig.screenHeight / 136.6),
-                            child: SizedBox(
-                              height: SizeConfig.screenHeight / 30,
-                              child:Text(
+                                    /// 16
+                                    fontWeight: FontWeight.w400),
+                              )))
+                          : SizedBox.shrink(),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: SizeConfig.screenHeight / 136.6),
+                        child: SizedBox(
+                            height: SizeConfig.screenHeight / 30,
+                            child: Text(
                               "${food.price}$current_string",
                               softWrap: true,
                               style: TextStyle(
@@ -115,41 +111,50 @@ Widget build_a_food_thumb(BuildContext context, PostData food) {
                                   fontSize: SizeConfig.screenHeight / 37.95,
                                   fontWeight: FontWeight.bold),
                             )),
-                          ),
-                        ],
                       ),
-                    ),
-                ],
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            food.isGood
+                ? Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                height: SizeConfig.screenHeight / 13.66,
+
+                /// 50.0
+                width: SizeConfig.screenWidth / 8.22,
+
+                /// 50.0
+                decoration: BoxDecoration(
+                    color: buttonColor,
+                    borderRadius: const BorderRadius.only(
+                      bottomRight: Radius.circular(30.0),
+                      topLeft: Radius.circular(30.0),
+                    )),
+                child: const Icon(
+                  Icons.shopping_cart_rounded,
+                  color: Colors.white,
+                ),
               ),
-              food.isGood
-                  ? Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: Container(
-                        height: SizeConfig.screenHeight / 13.66,
-
-                        /// 50.0
-                        width: SizeConfig.screenWidth / 8.22,
-
-                        /// 50.0
-                        decoration: BoxDecoration(
-                            color: buttonColor,
-                            borderRadius: const BorderRadius.only(
-                              bottomRight: Radius.circular(30.0),
-                              topLeft: Radius.circular(30.0),
-                            )),
-                        child: const Icon(
-                          Icons.shopping_cart_rounded,
-                          color: Colors.white,
-                        ),
-                      ),
-                    )
-                  : const SizedBox.shrink(),
-            ],
-          ),
+            )
+                : const SizedBox.shrink(),
+          ],
         ),
-      ],
-    ),
+      ),
+    ],
+  );
+}
+
+Widget build_a_food_thumb(BuildContext context, PostData food) {
+  return GestureDetector(
+    onTap: () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => PostDetailView(food: food)));
+    },
+    child: build_a_food_thumb_no_tap(food),
   );
 }
 
--- lib/ui/screens/discovery/map_discovery.dart
+++ lib/ui/screens/discovery/map_discovery.dart
@@ -6,6 +6,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:foodnet_01/ui/components/food_thumb.dart';
 import 'package:foodnet_01/ui/screens/discovery/map_style.dart';
 import 'package:foodnet_01/ui/screens/post_detail/post_detail.dart';
 import 'package:foodnet_01/util/constants/colors.dart';
@@ -100,160 +101,8 @@ class _DiscoveryState extends State<Discovery> {
                   MaterialPageRoute(
                       builder: (context) => PostDetailView(food: food)));
             },
-            child: Stack(
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(
-                      SizeConfig.screenWidth / 34.25,
-
-                      /// 12.0
-                      SizeConfig.screenHeight / 113.84,
-
-                      /// 6.0
-                      SizeConfig.screenWidth / 34.25,
-
-                      /// 12.0
-                      SizeConfig.screenHeight / 22.77
-
-                      /// 30.0
-                      ),
-                  height: SizeConfig.screenHeight / 3.105,
-
-                  /// 220.0
-                  width: SizeConfig.screenWidth / 2.74,
-
-                  /// 150.0
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30.0),
-                      boxShadow: [
-                        BoxShadow(
-                          offset: const Offset(0, 3),
-                          blurRadius: 6,
-                          color: Colors.black.withOpacity(0.3),
-                        )
-                      ]),
-                  child: Stack(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: SizeConfig.screenHeight / 6.83,
-
-                            /// 100.0
-                            width: SizeConfig.screenWidth / 2.74,
-
-                            /// 150.0
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(food.outstandingIMGURL),
-                                fit: BoxFit.cover,
-                              ),
-                              borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(30.0),
-                                  topRight: Radius.circular(30.0)),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                FittedBox(
-                                    fit: BoxFit.fitWidth,
-                                    child: Text(
-                                      food.title,
-                                      style: TextStyle(
-                                          color: Colors.black54,
-                                          fontSize:
-                                              SizeConfig.screenHeight / 34.15,
-
-                                          /// 20
-                                          fontWeight: FontWeight.w700),
-                                    )),
-                                FutureBuilder<ProfileData?>(
-                                    future: food.getOwner(),
-                                    builder: (context, snap) => snap.hasData
-                                        ? FittedBox(
-                                            fit: BoxFit.fitWidth,
-                                            child: Text(snap.data?.name ?? None,
-                                                style: TextStyle(
-                                                    color: Colors.green,
-                                                    fontSize: SizeConfig
-                                                            .screenHeight /
-                                                        40,
-                                                    fontFamily: "Roboto")))
-                                        : const SizedBox.shrink()),
-                                food.cateList.isNotEmpty
-                                    ? FittedBox(
-                                        fit: BoxFit.fitWidth,
-                                        child: Text(
-                                          food.cateList.join(','),
-                                          style: TextStyle(
-                                              color: Colors.black38,
-                                              fontSize:
-                                                  SizeConfig.screenHeight /
-                                                      42.69,
-
-                                              /// 16
-                                              fontWeight: FontWeight.w400),
-                                        ),
-                                      )
-                                    : const SizedBox.shrink(),
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      top: SizeConfig.screenHeight / 136.6),
-
-                                  /// 5.0
-                                  child: food.isGood
-                                      ? Text(
-                                          "\$${food.price}",
-                                          style: TextStyle(
-                                              color: buttonColor,
-                                              fontSize:
-                                                  SizeConfig.screenHeight /
-                                                      37.95,
-
-                                              /// 18
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      : const SizedBox.shrink(),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      food.isGood
-                          ? Positioned(
-                              bottom: 0,
-                              right: 0,
-                              child: Container(
-                                height: SizeConfig.screenHeight / 13.66,
-
-                                /// 50.0
-                                width: SizeConfig.screenWidth / 8.22,
-
-                                /// 50.0
-                                decoration: BoxDecoration(
-                                    color: buttonColor,
-                                    borderRadius: const BorderRadius.only(
-                                      bottomRight: Radius.circular(30.0),
-                                      topLeft: Radius.circular(30.0),
-                                    )),
-                                child: const Icon(
-                                  Icons.shopping_cart_rounded,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            )
-                          : const SizedBox.shrink(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+            child:
+            build_a_food_thumb_no_tap(food),
           );
         },
       ),
@@ -296,7 +145,7 @@ class _DiscoveryState extends State<Discovery> {
           title: Padding(
             padding: EdgeInsets.only(left: width / 7.4),
             child: Text(
-              "Map discovery",
+              discoveryString,
               style: TextStyle(
                 fontSize: height / 30.464, ///28
                 fontWeight: FontWeight.bold,
